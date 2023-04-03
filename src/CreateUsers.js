c75bafc9ef838ecf8ff8d78ffc1e7ead31ee3594
vengateshwaran <vengateshwaran.1994@gmail.com>
2023-04-04 02:06:01 +0530

    p

--- src/CreateUsers.js
+++ src/CreateUsers.js
@@ -5,6 +5,7 @@ import {useNavigate} from 'react-router-dom'
 export default function CreateUsers(){
   const nav = useNavigate();
   const [user, setUser] = useState({});
+  return(
         <div  className='EditUser'>
     <Container>       
         <Form>
