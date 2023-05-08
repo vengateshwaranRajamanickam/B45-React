c75c38fbac97005fe951d32ead4f59d3279a6869
vengateshwaran <vengateshwaran.1994@gmail.com>
2023-05-08 09:45:14 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Loginpage from './Component/Loginpage'
 import Forgotpassword from './Component/Forgotpassword';
 import Signuppage from './Component/Signuppage'
 import Verification from './Component/Verification'
+import Home from './Component/Home'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
 <BrowserRouter>
 <Routes>
+  <Route  path='/' element={<Home/>}/>
   <Route  path='/signup' element={<Signuppage/>}/>
   <Route path="/login" element={<Loginpage/>}/>
   <Route  path='/forgotpassword' element={<Forgotpassword/>}/>
---
+++ src/Component/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container } from "reactstrap";
+export default function Home(){
+    return(
+        <Container>
+<h3>user login system</h3>
+<Link to="/login">login page</Link><br></br>
+<Link to="/sign">sign page</Link><br></br>
+<Link to="/forgotpassword">forgot page</Link><br></br>
+<Link to="/verification">verification page</Link><br></br>
+</Container>
+    )
+}
